6d8309aa201cbb0cd4308a79f13981eed0dd8e23
Kirsty Brown <kirstyannbrown4@gmail.com>
2023-03-02 14:41:24 +0200

    models

--- db/migrate/20230302124010_create_bookmarks.rb
+++ db/migrate/20230302124010_create_bookmarks.rb
@@ -2,8 +2,9 @@ class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
       t.string :comment
-      t.integer :movie_id
-      t.integer :lists_id
+      t.references :list, null: false, foreign_key: true
+      t.references :movie, null: false, foreign_key: true
+
       t.timestamps
     end
   end
