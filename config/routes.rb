389aba6b29dcf2e9e6c38c045125e749a823dd83
Kirsty Brown <kirstyannbrown4@gmail.com>
2023-03-03 12:37:34 +0200

    Watch List methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  # get 'lists/new'
+  # get 'movies/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #root "lists#index"
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
